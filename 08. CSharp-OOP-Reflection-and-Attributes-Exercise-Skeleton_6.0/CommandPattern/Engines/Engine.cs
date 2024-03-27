95d3a2a5fce334ba66fdd86b36d87e942b394097
ivalcheff <iva.valcheff@gmail.com>
2024-03-27 08:34:38 -0700

    Reflection-exercise

---
+++ 08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/CommandPattern/Engines/Engine.cs
@@ -0,0 +1,37 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Engines
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string result = String.Empty;
+
+                try
+                {
+                    result = commandInterpreter.Read(input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ 08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/CommandPattern/Interpreters/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Interpreters
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] parts = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string command = $"{parts[0]}Command";
+            var commandArgs = parts.Skip(1).ToArray();
+
+            //взимаме сегашното Assembly и в него ще търсим необходимите типове
+            var assembly = Assembly.GetEntryAssembly();
+
+            //намираме типа със същото име като подадената команда
+            Type type = assembly?.GetTypes().FirstOrDefault(t => t.Name == command);
+
+            if (type ==  null)
+            {
+                throw new ArgumentException("Invalid type");
+            }
+
+            //правим инстанция на типа, за да можем да му ползваме метода
+            var instance = (ICommand)Activator.CreateInstance(type) ;
+            return instance?.Execute(commandArgs);        
+        }
+    }
+}
